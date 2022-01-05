057be061204d53582a08c4d9d95413470421d2c6
gaston <gaston.sp.92@hotmail.com>
2022-01-05 13:33:58 -0300

    werty gato

--- src/App.js
+++ src/App.js
@@ -10,8 +10,8 @@ function App() {
   return (
 
     <div className="App">
-     <Mensaje color='red'title='Wertyland' />
-    <Mensaje color='blue' title='estamos trabajando en un curso de react' />
+     <Mensaje color='red'title='Wertyland soy gato y me la como' />
+    <Mensaje color='blue' title='estamos trabajando en un curso de react aguarde un momento nuestros operadores estan ocupados intenten nuevamente luego muchas gracias.' />
   
 
     </div>
